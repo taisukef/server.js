9794b96a857bd27f1aed50ecc7a34afca0ec4a47
Taisuke Fukuno <fukuno@jig.jp>
2021-04-24 08:23:51 +0900

    add highscore-api

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 import { Server } from "https://js.sabae.cc/Server.js";
-new Server(8001);
+
+const scores = [];
+
+class MyServer extends Server {
+  api(path, req) {
+    if (path == "/api/list") {
+      return scores;
+    } else if (path == "/api/add") {
+      scores.push(req);
+      scores.sort((a, b) => b.score - a.score);
+      return "ok";
+    }
+  }
+};
+new MyServer(8001);
