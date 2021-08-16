684cd452baf4a5974943e00d5889cae550f12544
Taisuke Fukuno <fukuno@jig.jp>
2021-08-16 12:45:38 +0900

    fix doc

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ import { Server } from "https://js.sabae.cc/Server.js";
 const scores = [];
 
 class MyServer extends Server {
-  api(path, req) {
+  api(path, req, remoteAddr) {
+    console.log(path, req);
     if (path == "/api/list") {
       return scores;
     } else if (path == "/api/add") {
       scores.push(req);
       scores.sort((a, b) => b.score - a.score);
       return "ok";
+    } else {
+      console.log(remoteAddr);
+      return { message: "Hello! your IP is: ", remoteAddr, date: new Date() };
     }
   }
 };
