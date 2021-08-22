fb21819342edf4689bfce408c04a13fdc95a8387
Taisuke Fukuno <fukuno@jig.jp>
2021-08-22 20:30:28 +0900

    ログイン機能追加

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 import { Server } from "https://js.sabae.cc/Server.js";
-import { jsonfs } from "https://js.sabae.cc/jsonfs.js";
+import { JSONDB } from "https://js.sabae.cc/JSONDB.js";
 
-const datafn = "data.json";
-let data = jsonfs.read(datafn) || [];
+const bbs = new JSONDB("bbs.json");
+const user = new JSONDB("user.json");
 
 class MyServer extends Server {
   api(path, req) {
     if (path == "/api/list") {
-      return data;
+      return bbs.data;
     } else if (path == "/api/add") {
-      data.push(req);
-      jsonfs.write(datafn, data);
+      const u = user.data.find(d => d.name == req.name && d.session == req.session);
+      if (!u) {
+        return "err";
+      }
+      delete req.session;
+      bbs.data.push(req);
+      bbs.write();
       return "ok";
+    } else if (path == "/api/regist") {
+      const u = user.data.find(d => d.name == req.name);
+      if (!u) {
+        req.session = Math.random();
+        user.data.push(req);
+        user.write();
+        return req.session;
+      }
+      if (u.password != req.password) {
+        return null; // wrong password
+      }
+      return u.session;
     }
   }
 }
